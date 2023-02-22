57f24eb99ab0cd769d6b8d700dbc489f80854f26
katefranchuk <105155465+katefranchuk@users.noreply.github.com>
2023-02-22 16:11:12 +0000

    Deploying to gh-pages from @ katefranchuk/goit-react-hw-05-movies@59f025691d5bef9c25ef2fbb938a5ef4cb53e7bd 🚀

---
+++ static/js/520.1c289a78.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[520],{4520:function(e,t,s){s.r(t);var i=s(1087),n=s(184);t.default=function(){return(0,n.jsxs)("div",{style:{textAlign:"center"},children:["This page doesn't exist. Go ",(0,n.jsx)(i.rU,{to:"/",children:"home"}),"."]})}}}]);
+//# sourceMappingURL=520.1c289a78.chunk.js.map
